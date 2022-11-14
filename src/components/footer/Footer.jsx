4526a7f3cdea3cbd375b18f7c3dcdbb1e81c8014
DinhNgocThai <56432733+Sharrfire@users.noreply.github.com>
2022-11-14 11:41:47 +0700

    Prepare for project

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+Footer.propTypes = {
+    
+};
+
+function Footer(props) {
+    return (
+        <div>
+            This is footer
+        </div>
+    );
+}
+
+export default Footer;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+Header.propTypes = {
+    
+};
+
+function Header(props) {
+    return (
+        <div>
+            This is header
+        </div>
+    );
+}
+
+export default Header;
