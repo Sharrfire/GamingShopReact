8d3b55b25511f1746697809a3c235be4ed4eed3c
DinhNgocThai <56432733+Sharrfire@users.noreply.github.com>
2022-11-16 18:51:07 +0700

    Design header navigation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import 'normalize.css'
 import './App.css';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
+import './assets/css/reset.css'
+import './assets/css/main.css'
+
 function App() {
   return (
     <div className="App">
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,60 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import PersonIcon from "@mui/icons-material/Person";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import classNames from "classnames/bind";
+import styles from "./Header.module.scss";
+import img from "./logo.png";
 
-Header.propTypes = {
-    
-};
+Header.propTypes = {};
+const cx = classNames.bind(styles);
 
 function Header(props) {
-    return (
-        <div>
-            This is header
+  return (
+    <div className={cx("header-container")}>
+      <div className={cx("header-contact-top")}>
+        <div className={cx("header-contact-container")}>
+          <div className={cx("header-contact-left")}>
+            <ul className={cx("header-contact-icon")}>
+              <li>
+                <YouTubeIcon />
+              </li>
+            </ul>
+          </div>
+          <div className={cx("header-contact-right")}>
+            <ul className={cx("header-contact-icon")}>
+              <li>
+                <PersonIcon />
+              </li>
+              <li>
+                <ShoppingCartIcon />
+              </li>
+            </ul>
+          </div>
         </div>
-    );
+      </div>
+
+      <div className={cx("header-navbar")}>
+        <div className={cx("header-navbar-container")}>
+          <div className={cx("header-left")}>
+            <img src={img} alt="Logo" />
+          </div>
+          <div className={cx("header-right")}>
+            <ul className={cx("header-nav-list")}>
+              <li>
+                <a href="/">GALLERY</a>
+              </li>
+              <li>
+                <a href="/">CART</a>
+              </li>
+              <li>
+                <a href="/">CHECKOUTS</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
